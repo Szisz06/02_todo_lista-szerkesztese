a3196e459d96c02091f2f1eb0d462d7ed0eaa32e
Szisz06 <moczaszilvia@gmail.com>
2023-10-14 11:56:08 +0200

    ORA-VEGE

--- MegjelenitSor.js
+++ MegjelenitSor.js
@@ -1,38 +1,53 @@
 class MegjelenitSor {
-    #adat = {};
- 
-    constructor(adat, szuloElem) {
-        this.#adat = adat;
-   
-        this.tablaElem = szuloElem;
+  #adat = {};
 
-        this.#sor();
-        /** eseménykezelők a kész és a törlés gombokhoz */
-        this.sorElem = this.tablaElem.children("tr:last-child");
-        this.keszElem = this.sorElem.children("td").children(".kesz");
-        this.torolElem = this.sorElem.children("td").children(".torol");
-     
-      
-        //console.log(this.keszElem);
-        this.keszElem.on("click", () => {
-            console.log(this)
-        });
-       
-       
+  constructor(adat, szuloElem, index) {
+    this.index = index;
+    this.#adat = adat;
+
+    this.tablaElem = szuloElem;
+
+    this.#sor();
+    /** eseménykezelők a kész és a törlés gombokhoz */
+    this.sorElem = this.tablaElem.children("tr:last-child");
+    this.keszElem = this.sorElem.children("td").children(".kesz");
+    this.torolElem = this.sorElem.children("td").children(".torol");
+    if (this.#adat.kesz){
+        this.setHatterSzin();
     }
-    #sor() {
-        let txt = "";
 
-        txt += "<tr>";
-        for (const key in this.#adat) {
-            txt += `<td>${this.#adat[key]}</td>`;
-          }
-        
-        txt += `<td><span class="kesz">✔️</span> <span class="torol">🗑</span></td>`;
-        txt += "</tr>";
+    //console.log(this.keszElem);
+    this.keszElem.on("click", () => {
+      //console.log(this)
+      this.#esemyenyTrigger("kesz");
+    });
+    this.torolElem.on("click", () => {
+      //console.log(this)
+      this.#esemyenyTrigger("torol");
+    });
+  }
+  setHatterSzin() {
+    this.sorElem.css("background-color", "green");
+  }
+  #sor() {
+    let txt = "";
 
-        this.tablaElem.append(txt);
+    txt += "<tr>";
+    for (const key in this.#adat) {
+      if (key != "kesz") {
+        txt += `<td>${this.#adat[key]}</td>`;
+      }
     }
-   
+
+    txt += `<td><span class="kesz">✔️</span> <span class="torol">🗑</span></td>`;
+    txt += "</tr>";
+
+    this.tablaElem.append(txt);
+  }
+
+  #esemyenyTrigger(esemenyNev) {
+    const e = new CustomEvent(esemenyNev, { detail: this });
+    window.dispatchEvent(e);
+  }
 }
 export default MegjelenitSor;
--- main.js
+++ main.js
@@ -1,10 +1,20 @@
 import { TODOLIST2 } from "./adatok.js";
 import Megjelenit from "./Megjelenit.js";
 $(function () {
-    const szuloELEM = $(".tarolo");
-    new Megjelenit(TODOLIST2, szuloELEM);
+  const szuloELEM = $(".tarolo");
+  new Megjelenit(TODOLIST2, szuloELEM);
 
-    $(window).on("torles", (event) => {
-        console.log(event.detail);
-    });
+  $(window).on("kesz", (event) => {
+    let objPeldany = event.detail;
+    objPeldany.setHatterSzin();
+    console.log(TODOLIST2);
+    TODOLIST2[objPeldany.index].kesz = true;
+  });
+
+  $(window).on("torol", (event) => {
+    let Peldany = event.detail;
+    TODOLIST2.splice(Peldany.index, 1);
+    szuloELEM.empty();
+    new Megjelenit(TODOLIST2, szuloELEM);
+  });
 });
